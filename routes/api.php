45f2d0a4b1e3f306b25b137e9f96c94f36b27a1c
Luis Fernando Ventocilla <1507430@senati.pe>
2025-02-04 13:14:44 -0500

    Routes the Product

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
 use App\Http\Middleware\Authentication;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -12,3 +13,14 @@ Route::get('/user', function (Request $request) {
 Route::post('/login',[AuthController::class,'login'])->withoutMiddleware(Authentication::class);
 Route::post('/logout',[AuthController::class,'logout']);
 Route::get('/prueba',[AuthController::class,'prueba']);
+
+Route::group([
+    'prefix' => 'product',
+    'controller' => ProductController::class
+], static function(){
+    Route::get('/','getProducts');
+    Route::get('/{productId}','getProduct');
+    Route::post('/','store');
+    Route::put('/{productId}','update');
+    Route::delete('/{productId}','delete');
+});
